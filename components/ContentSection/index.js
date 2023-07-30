22a544dfcf79337b6bfbebadcaffbd25da986b4b
Mohit <mohit.ranjan@lybrate.com>
2023-07-30 14:00:31 +0530

    homepage design

---
+++ components/ContentSection/index.js
@@ -0,0 +1,15 @@
+import styles from './contentSection.module.scss'
+
+const ContentSection = () => {
+  return (
+    <div className={styles.container}>
+      <h1>
+        Instant Portfolio Websites: Effortless, Personalized, and Stunning!
+        Create Your Dream Portfolio with Ease. No Coding Required.
+      </h1>
+      <button>Get Started Now!</button>
+    </div>
+  )
+}
+
+export default ContentSection
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
 import '@/styles/globals.css'
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+import PageTransition from '../components/PageTransition'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  return (
+    <motion.div
+      key={router.route} // Ensure the route change triggers the animation
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        }
+      }}
+    >
+      <PageTransition>
+        <Component {...pageProps} />
+      </PageTransition>
+    </motion.div>
+  )
 }
+
+export default MyApp
