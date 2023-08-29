4fd695b5977d8ff1958378df600af0bba4bb86d4
nosangdev <nosangdev@gmail.com>
2023-08-29 20:13:04 +0530

    added loading dialog component

--- components/InsertPortfolioDataForm/index.js
+++ components/InsertPortfolioDataForm/index.js
@@ -8,9 +8,11 @@ import { saveFormData } from "@/utilities/submitForm";
 import { useToast } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { getPortfolioByEmail } from "@/common/api";
+import LoadingDialog from "../common/LoadingDialog";
 
 const InsertPortfolioDataForm = ({ prop }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [dataFetching, setDataFetching] = useState(false);
   const toast = useToast({ position: "top-right" });
   const session = useSession();
 
@@ -29,9 +31,9 @@ const InsertPortfolioDataForm = ({ prop }) => {
   useEffect(() => {
     // fetch portfolio data
     if (session?.data?.user?.email) {
-      setIsLoading(true);
+      setDataFetching(true);
       getPortfolioByEmail(session?.data?.user?.email).then((res) => {
-        setIsLoading(false);
+        setDataFetching(false);
         if (res?.status === 200) {
           const data = res.data;
           let newListItems = [];
@@ -63,6 +65,7 @@ const InsertPortfolioDataForm = ({ prop }) => {
         profession: "",
         listItems: [{ title: "", desc: "" }],
       });
+      setDataFetching(false);
     };
   }, [session]);
 
@@ -220,70 +223,73 @@ const InsertPortfolioDataForm = ({ prop }) => {
   }, [formData]);
 
   return (
-    <div className={`tw-px-2 ${styles.formContainer}`}>
-      <h1 className="tw-text-center tw-text-lg tw-font-bold">
-        This is where you decide what you want to tell your audience!
-      </h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <Input
-          required
-          className="tw-mt-2"
-          variant={Boolean(errorData?.name) ? "error" : "bordered"}
-          type="text"
-          value={formData.name}
-          onChange={handleNameChange}
-          placeHolder="Enter Your Display Name"
-          errorMessage={errorData?.name}
-        />
-        <Input
-          className="tw-mt-2"
-          variant={Boolean(errorData?.profession) ? "error" : "bordered"}
-          type="text"
-          value={formData.profession}
-          onChange={handleAgeChange}
-          placeHolder="Enter Your Profession"
-          errorMessage={errorData?.profession}
-        />
-        <p className="tw-mt-1 tw-text-center">
-          This (these) section(s) will have a list of what contents you are
-          going to display as list titles and descriptions
-        </p>
-        {formData.listItems.map((item, index) => {
-          return (
-            <NavItemInputs
-              titleError={errorData?.listItems[index]}
-              item={item}
-              key={index}
-              index={index}
-              handleRemoveComponent={handleRemoveComponent}
-              handleTitleChange={handleTitleChange}
-              handleDescChange={handleDescChange}
-            />
-          );
-        })}
+    <>
+      <LoadingDialog isOpen={dataFetching} />
+      <div className={`tw-px-2 ${styles.formContainer}`}>
+        <h1 className="tw-text-center tw-text-lg tw-font-bold">
+          This is where you decide what you want to tell your audience!
+        </h1>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Input
+            required
+            className="tw-mt-2"
+            variant={Boolean(errorData?.name) ? "error" : "bordered"}
+            type="text"
+            value={formData.name}
+            onChange={handleNameChange}
+            placeHolder="Enter Your Display Name"
+            errorMessage={errorData?.name}
+          />
+          <Input
+            className="tw-mt-2"
+            variant={Boolean(errorData?.profession) ? "error" : "bordered"}
+            type="text"
+            value={formData.profession}
+            onChange={handleAgeChange}
+            placeHolder="Enter Your Profession"
+            errorMessage={errorData?.profession}
+          />
+          <p className="tw-mt-1 tw-text-center">
+            This (these) section(s) will have a list of what contents you are
+            going to display as list titles and descriptions
+          </p>
+          {formData.listItems.map((item, index) => {
+            return (
+              <NavItemInputs
+                titleError={errorData?.listItems[index]}
+                item={item}
+                key={index}
+                index={index}
+                handleRemoveComponent={handleRemoveComponent}
+                handleTitleChange={handleTitleChange}
+                handleDescChange={handleDescChange}
+              />
+            );
+          })}
 
-        <Button
-          className="tw-ml-auto tw-mt-2"
-          type="bordered"
-          onClick={handleAddComponent}
-        >
-          Add Component
-        </Button>
+          <Button
+            className="tw-ml-auto tw-mt-2"
+            type="bordered"
+            onClick={handleAddComponent}
+          >
+            Add Component
+          </Button>
 
-        <Button
-          isDisabled={isLoading}
-          loading={isLoading}
-          btnType="submit"
-          className={`tw-w-full tw-mt-2 tw-sticky tw-bottom-0 ${styles.submitButton}`}
-          type="bordered"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          Submit
-        </Button>
-      </form>
-    </div>
+          <Button
+            isDisabled={isLoading}
+            loading={isLoading}
+            btnType="submit"
+            className={`tw-w-full tw-mt-2 tw-sticky tw-bottom-0 ${styles.submitButton}`}
+            type="bordered"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </>
   );
 };
 export default InsertPortfolioDataForm;
